1a7edf991bc22ef757ac16f0a596f04b7821d9e9
Alexander Egeberg <alexanderegeberg@hotmail.com>
2022-09-18 21:24:29 +0200

    parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,37 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
 
   return (
-    <section className='flex justify-center bg-slate-900 py-44'>
-      <figure>
-        <Image
-          className='rounded-full'
-          src='/images/profile.png'
-          height={200}
-          width={200}
-          alt='Alexander profile picture'
-        />
+    <>
+      <section
+        style={{backgroundImage: `url(https://www.kindacode.com/wp-content/uploads/2022/05/orange-background.jpeg)`}}
+        className='flex justify-center py-44 bg-fixed bg-center bg-repeat bg-cover'
+      >
+        <figure>
+          <Image
+            className='rounded-full'
+            src='/images/profile.png'
+            height={200}
+            width={200}
+            alt='Alexander profile picture'
+          />
 
-        <h2 className='flex justify-center text-zinc-300'>
+          <h2 className='flex justify-center text-zinc-300'>
           Alexander
-        </h2>
-      </figure>
-    </section>
+          </h2>
+        </figure>
+      </section>
+
+      <section className='flex justify-center pt-96 bg-gray-700'>
+        testhelo wtf?
+
+      </section>
+
+      <section className='flex justify-center pt-96'>
+        testhelo wtf?
+
+      </section>
+
+    </>
   )
 }
 
